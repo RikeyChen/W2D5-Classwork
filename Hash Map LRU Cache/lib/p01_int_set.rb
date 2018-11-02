cc34dd068afe43b619d484eeaaa12a52fcfc4f11
rikeychen <rikey.chen16@gmail.com>
2018-11-02 14:03:40 -0700

    Add MaxIntSet and IntSet

--- Hash Map LRU Cache/lib/p01_int_set.rb
+++ Hash Map LRU Cache/lib/p01_int_set.rb
@@ -1,14 +1,21 @@
 class MaxIntSet
   def initialize(max)
+    @max = max
+    @store = []
   end
 
   def insert(num)
+    raise "Out of bounds" unless num.between?(0, @max - 1)
+    @store[num] = true
   end
 
   def remove(num)
+    raise "Number not found" unless num.between?(0, @max - 1)
+    @store[num] = false
   end
 
   def include?(num)
+    @store[num] == true
   end
 
   private
@@ -27,18 +34,22 @@ class IntSet
   end
 
   def insert(num)
+    self[num] << num unless self.include?(num)
   end
 
   def remove(num)
+    self[num].delete(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
