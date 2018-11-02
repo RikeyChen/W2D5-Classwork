1b2f283501e14bb808818bd55fcec4751298ea30
rikeychen <rikey.chen16@gmail.com>
2018-11-02 14:39:20 -0700

    Add ResizingIntSet class

--- Hash Map LRU Cache/lib/p01_int_set.rb
+++ Hash Map LRU Cache/lib/p01_int_set.rb
@@ -66,18 +66,30 @@ class ResizingIntSet
   end
 
   def insert(num)
+    unless self.include?(num)
+      self[num] << num 
+      @count += 1
+    end
+    
+    resize! if @count > num_buckets
   end
 
   def remove(num)
+    if self.include?(num)
+      self[num].delete(num)
+      @count -= 1
+    end
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -85,5 +97,11 @@ class ResizingIntSet
   end
 
   def resize!
+    prev_store = @store
+    @store = Array.new(num_buckets * 2) { Array.new }
+    
+    prev_nums = prev_store.flatten
+    @count = 0
+    prev_nums.each {|num| insert(num)}
   end
 end
