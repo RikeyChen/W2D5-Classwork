5c876daf93414e298d4f21e3212c2f5fe934c1ff
rikeychen <rikey.chen16@gmail.com>
2018-11-02 15:38:40 -0700

    Add p02_hashing.rb and p03_hash_set.rb

--- Hash Map LRU Cache/lib/p02_hashing.rb
+++ Hash Map LRU Cache/lib/p02_hashing.rb
@@ -1,21 +1,32 @@
-class Fixnum
-  # Fixnum#hash already implemented for you
-end
+# class Fixnum
+#   # Fixnum#hash already implemented for you
+# end
+# 
+# class Array
+#   def hash
+#     current_xor = self[0]
+#     self.each_index do |i|
+#       current_xor = current_xor ^ self[i+1] unless i == self.length - 1
+#     end
+# 
+#     current_xor.hash
+#   end
+# end
+# 
+# class String
+#   def hash
+#   end
+# end
+# 
+# class Hash
+#   # This returns 0 because rspec will break if it returns nil
+#   # Make sure to implement an actual Hash#hash method
+#   def hash
+#     0
+#   end
+# end
 
-class Array
-  def hash
-  end
-end
 
-class String
-  def hash
-  end
-end
 
-class Hash
-  # This returns 0 because rspec will break if it returns nil
-  # Make sure to implement an actual Hash#hash method
-  def hash
-    0
-  end
-end
+# sum
+# sum += item.hash ^ i
--- Hash Map LRU Cache/lib/p03_hash_set.rb
+++ Hash Map LRU Cache/lib/p03_hash_set.rb
@@ -6,19 +6,31 @@ class HashSet
     @count = 0
   end
 
-  def insert(key)
+  def insert(key)    
+    unless self.include?(key.hash)
+      self[key.hash] << key
+      @count += 1
+    end
+    
+    resize! if @count > num_buckets
   end
 
   def include?(key)
+    self[key.hash].include?(key)
   end
 
   def remove(key)
+    if self.include?(key)
+      self[key.hash].delete(key)
+      @count -= 1
+    end
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -26,5 +38,11 @@ class HashSet
   end
 
   def resize!
+    prev_store = @store
+    @store = Array.new(num_buckets * 2) { Array.new }
+    
+    prev_nums = prev_store.flatten
+    @count = 0
+    prev_nums.each {|key| insert(key)}
   end
 end
