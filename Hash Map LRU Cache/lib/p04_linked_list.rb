9f3b369a4104b2f702784942a694fa693c486cf0
rikeychen <rikey.chen16@gmail.com>
2018-11-02 18:15:40 -0700

    Add #get, #each, #append, and #update to p04_linked_list.rb' git commit -m Add

--- Hash Map LRU Cache/lib/p04_linked_list.rb
+++ Hash Map LRU Cache/lib/p04_linked_list.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 class Node
   attr_reader :key
   attr_accessor :val, :next, :prev
@@ -20,39 +22,88 @@ class Node
 end
 
 class LinkedList
+  include Enumerable
+  
   def initialize
+    @head = Node.new
+    @tail = Node.new
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
     each_with_index { |link, j| return link if i == j }
     nil
-  end
+  end 
 
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    @head.next == @tail
   end
 
   def get(key)
+    return first.val if first.key == key
+      
+    current_node = first
+    until current_node == @tail
+      if current_node.next.key == key
+        return current_node.next.val
+      else
+        current_node = current_node.next
+      end
+    end
+    nil
   end
 
   def include?(key)
+    get(key) ? true : false
   end
 
   def append(key, val)
+    new_node = Node.new(key, val)
+    prev_node = @tail.prev
+    
+    new_node.prev = prev_node  #this set's new_node's prev to prev_node
+    new_node.next = @tail      #this set's new_node's next to tail
+    prev_node.next = new_node  #this sets prev_node's next to new node
+    @tail.prev = new_node      #this set's tail's prev to new_node
   end
 
   def update(key, val)
+    self[key]
   end
 
   def remove(key)
+    current_node = first
+    
+    until current_node == @tail
+      if current_node.key == key
+        current_node.prev.next = current_node.next
+        current_node.next.prev = current_node.prev
+        current_node.next = nil
+        current_node.prev = nil
+      else
+        current_node = current_node.next
+      end
+    end
+    nil
   end
 
-  def each
+  def each(&prc)
+    current_node = first
+    until current_node == @tail
+      prc.call(current_node)
+      current_node = current_node.next
+    end
+    
+    self
   end
 
   # uncomment when you have `each` working and `Enumerable` included
